c4e968ec1823e00eeb46c8a633179abaf1fbe6ee
hackycy <qa894178522@qq.com>
2019-12-12 11:39:22 +0800

    feature：引入Vuex和Vue-router

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
 
 new Vue({
   render: h => h(App),
