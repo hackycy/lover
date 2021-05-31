120f08b6875b0a89bffba93a76675272b53cb34c
hackycy <qa894178522@qq.com>
2021-05-31 16:37:10 +0800

    feat：add babel

--- src/plugins/vant.ts
+++ src/plugins/vant.ts
@@ -2,11 +2,6 @@ import { App } from "vue"
 import '@vant/touch-emulator'
 import { Swipe, SwipeItem, Toast, Lazyload } from 'vant'
 
-import 'vant/es/swipe/style';
-import 'vant/es/swipe-item/style'
-import 'vant/es/toast/style'
-import 'vant/es/lazyload/style'
-
 const components = [ Swipe, SwipeItem, Toast ]
 
 export default {
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,16 @@ export default defineConfig({
       }
     ]
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'vant',
+          esModule: true,
+          resolveStyle: (name) => `vant/es/${name}/style`
+        }
+      ]
+    })
+  ]
 })
