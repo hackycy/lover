55de53a7b4bd72eae99ceccc3590a6ddbbcdf4fe
hackycy <qa894178522@qq.com>
2021-05-31 16:18:44 +0800

    feat：import css

--- src/plugins/vant.ts
+++ src/plugins/vant.ts
@@ -1,13 +1,20 @@
 import { App } from "vue"
 import '@vant/touch-emulator'
-import { Swipe, SwipeItem } from 'vant'
+import { Swipe, SwipeItem, Toast, Lazyload } from 'vant'
 
-const components = [ Swipe, SwipeItem ]
+import 'vant/es/swipe/style';
+import 'vant/es/swipe-item/style'
+import 'vant/es/toast/style'
+import 'vant/es/lazyload/style'
+
+const components = [ Swipe, SwipeItem, Toast ]
 
 export default {
   install: function(app: App) {
+    // component
     components.forEach((comp) => {
       app.component(comp.name, comp)
     })
+    app.use(Lazyload)
   }
 }
